8799ec031b2295ebf2a7489e4a15d127b02f73be
june1921 <zone1921@naver.com>
2023-08-30 14:10:23 +0900

    단위 테스트

--- cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -1,10 +1,16 @@
 package com.packt.cardatabase;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.packt.cardatabase.web.CarController;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
+	
+	@Autowired
+	private CarController controller;
 
 	@Test
 	void contextLoads() {
