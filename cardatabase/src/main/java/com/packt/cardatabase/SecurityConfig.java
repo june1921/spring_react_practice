fa662aa4ab9c71ea1effc8df70b541b42685675f
ASTEMS <ASTEMS@DESKTOP-IFO393S>
2023-08-30 11:27:18 +0900

    CORS 필터 추가

--- cardatabase/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.packt.cardatabase;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -21,15 +26,23 @@ public class SecurityConfig {
 	@Autowired
 	private AuthenticationFilter authenticationFilter;
 	
+	@Autowired
+	private AuthEntryPoint exceptionHandler;
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		http.csrf().disable().cors().and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		.authorizeRequests()
 		// /login 엔드포인트에 대한 post 요청은 보호되지 않음
 		.requestMatchers(HttpMethod.POST, "/login").permitAll()
 		// 다른 모든 요청은 보호됨
 		.anyRequest().authenticated().and()
+		.exceptionHandling()
+		.authenticationEntryPoint(exceptionHandler).and()
 		.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
 		return http.build();
 	}
 
@@ -42,6 +55,22 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		// 이하는 명시적 허용
+		//config.setAllowedOrigins("http://localhost:3000")); 
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(false);
+		config.applyPermitDefaultValues();
+		
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 
 }
